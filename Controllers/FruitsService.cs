8bab1d120ad6050e4ebb49eb013dac9f16871ab0
amittpal <amitrailwayservice@gmail.com>
2026-05-24 19:14:44 +0530

    Initial commit

---
+++ Controllers/FruitsService.cs
@@ -0,0 +1,15 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace MyMCP.Controllers
+{
+    [McpServerToolType]
+    public class FruitsService
+    {
+        [McpServerTool, Description("Gets list of fruits")]
+        public string[] GetFruits()
+        {
+            return ["Mango", "Banana"];
+        }
+    }
+}
